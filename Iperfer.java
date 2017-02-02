9a3c583a3faef3e242d87dc4a31850c79b12bedd
Joanne Lee <jhlee44@wisc.edu>
2017-02-02 10:01:12 -0600

    slight edits

--- Iperfer.java
+++ Iperfer.java
@@ -3,12 +3,10 @@ import java.io.*;
 
 public class Iperfer {
 
-	public static String hostname;
-	public static int serverPort, listenPort, time;
 
 	public static void main(String[] args) {
 
-		//argument check
+		//client/server argument check
 		if (args[0].charAt(1) != 'c' && args[0].charAt(1) != 's')
 		{
 			System.out.println("Error: missing or additional arguments");
@@ -18,6 +16,8 @@ public class Iperfer {
 		//CLIENT
 		if (args[0].charAt(1) == 'c')
 		{
+			String hostname;
+			int time, serverPort;
 			if (args.length != 7 || args[1].charAt(1) != 'h' || args[3].charAt(1) != 'p' || args[5].charAt(1) != 't')
 
 			{
@@ -25,17 +25,23 @@ public class Iperfer {
 				return;
 			}
 
+			hostname = args[2];
+			time = Integer.parseInt(args[6]);
 			serverPort = Integer.parseInt(args[4]);
 			if (serverPort < 1024 || serverPort > 65535)
 			{
 				System.out.println(" Error: port number must be in the range 1024 to 65535");
 				return;
 			}
+			
+			
+			
 		}
 
 		//SERVER
 		else
 		{
+			int listenPort;
 			if (args.length != 3 || args[1].charAt(1) != 'p')
 			{
 				System.out.println("Error: missing or additional arguments");
